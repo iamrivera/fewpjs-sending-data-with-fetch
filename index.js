925dd9523d44de39e6b641f512636829730469c2
iamrivera <a3m3rivera@gmail.com>
2021-07-12 21:03:07 -0700

    commit tree 366a1861c4bbd4ad994a0bb3e970558e7f6d3476

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(userName, userEmail){
+    let userURL = 'http://localhost:3000/users'
+    let userConfigObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+            })
+        };
+    
+    return fetch(userURL, userConfigObject).then(function(response){
+        return response.json();
+    }).then(function(object){
+        document.body.innerHTML = object['id'];
+    }).catch(function(error){
+        alert("Ruh Roh Shaggy!");
+        document.body.innerHTML = error.message;
+    });
+};
+
+
+// Seek clarity as to why this wouldn't qualify the test despite displaying the id on page
+// const objectId = object['id'];
+// let h2 = document.createElement("h2");
+// h2.innerText = objectId;
+// document.body.appendChild(h2);
